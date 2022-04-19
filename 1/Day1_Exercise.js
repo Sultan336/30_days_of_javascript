894453a0321c462aae6799d2e52813583e031bfd
Sultan <m.sultan338@gmail.com>
2022-04-20 00:34:17 +0500

    Update Day1_Exercise.js

---
+++ 1/Day1_Exercise.js
@@ -0,0 +1,39 @@
+// Write a single line comment which says, comments can make code readable
+
+    // Comments can make code readable
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+    // Welcome to 30Days ofJavascript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+    /* comments can make code readable
+       easy to reuse
+       and informative */
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+    //Done
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+    //Done
+
+// Declare four variables without assigning values
+
+    //Done
+
+// Declare four variables with assigned values
+
+    //Done
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+    //Done
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+    //Done
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
