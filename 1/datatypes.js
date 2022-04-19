500ce99bcaa008cf074ba08331573e1882f38984
Sultan <m.sultan338@gmail.com>
2022-04-20 00:33:58 +0500

    day 1 complete

---
+++ 1/datatypes.js
@@ -0,0 +1,41 @@
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof 'Tuesday')
+console.log(typeof 2022)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof 3.124)
+console.log(typeof false)
+console.log(typeof [1,2,3,4])
+
+// Declare four variables without assigning values
+
+// let var_1
+// let var_2
+// let var_3
+// let var_4
+
+// Declare four variables with assigned values
+
+// let var_1 = 1
+// let var_2 = 2
+// let var_3 = 3
+// let var_4 = 4
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+    // let firstName = 'John'
+    // let lastName = 'Doe'
+    // let maritalStatus = 'Single'
+    // let country = 'USA'
+    // let age = '30'
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+ 
+    // let firstName = 'John' , lastName = 'Doe' , maritalStatus = 'Single' , country = 'USA' , age = '30'
+
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 25, yourAge = 20
+console.log('myAge', myAge, 'yourAge', yourAge)
---
+++ 1/variable.js
@@ -0,0 +1,7 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let day = 'Tuesday', month = 'April', year = '2022'
+let weather 
+let animal = null
+let is_sunny = true 
+
+console.log('day', day, 'month', month, 'year', year, 'weather', weather, 'animal', animal, 'is_sunny', is_sunny)
