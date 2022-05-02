dcfc8f1c68e306084fe9fe4b708282a6d3603f8e
Sultan <m.sultan338@gmail.com>
2022-05-02 12:35:39 +0500

    day 3

---
+++ 3/date_object.js
@@ -0,0 +1,16 @@
+//Date object 
+const now = new Date()
+console.log(`${now}
+${now.getFullYear()}\n
+${now.getDate()}\n
+${now.getDay()}\n
+${now.getHours()}\n
+${now.getMilliseconds()}\n
+${now.getMinutes()}\n
+${now.getMonth()}\n
+${now.getSeconds()}\n
+${now.getTime()}\n
+${now.getTimezoneOffset()}\n
+${now.getUTCDate()}\n
+${now.getUTCDay()}\n
+`)
---
+++ 3/day_3.js
@@ -0,0 +1,139 @@
+// Undefined
+let first_name
+console.log(first_name)
+
+//Null
+let empty = null
+console.log(null)
+
+// Arithmetic Operators
+let numOne = 4
+let numTwo = 3
+let sum = numOne + numTwo
+let diff = numOne - numTwo
+let mult = numOne * numTwo
+let div = numOne / numTwo
+let remainder = numOne % numTwo
+let powerOf = numOne ** numTwo
+
+console.log(sum, diff, mult, div, remainder, powerOf) // 7,1,12,1.33,1, 64
+
+const PI = 3.14
+let radius = 100          // length in meter
+
+//Let us calculate area of a circle
+const areaOfCircle = PI * radius * radius
+console.log(areaOfCircle)  //  314 m
+
+
+const gravity = 9.81      // in m/s2
+let mass = 72             // in Kilogram
+
+// Let us calculate weight of an object
+const weight = mass * gravity
+console.log(weight)        // 706.32 N(Newton)
+
+const boilingPoint = 100  // temperature in oC, boiling point of water
+const bodyTemp = 37       // body temperature in oC
+
+
+// Concatenating string with numbers using string interpolation
+/*
+ The boiling point of water is 100 oC.
+ Human body temperature is 37 oC.
+ The gravity of earth is 9.81 m/s2.
+ */
+console.log(
+  `The boiling point of water is ${boilingPoint} oC.\nHuman body temperature is ${bodyTemp} oC.\nThe gravity of earth is ${gravity} m / s2.`
+)
+
+console.log(3 > 2)              // true, because 3 is greater than 2
+console.log(3 >= 2)             // true, because 3 is greater than 2
+console.log(3 < 2)              // false,  because 3 is greater than 2
+console.log(2 < 3)              // true, because 2 is less than 3
+console.log(2 <= 3)             // true, because 2 is less than 3
+console.log(3 == 2)             // false, because 3 is not equal to 2
+console.log(3 != 2)             // true, because 3 is not equal to 2
+console.log(3 == '3')           // true, compare only value
+console.log(3 === '3')          // false, compare both value and data type
+console.log(3 !== '3')          // true, compare both value and data type
+console.log(3 != 3)             // false, compare only value
+console.log(3 !== 3)            // false, compare both value and data type
+console.log(0 == false)         // true, equivalent
+console.log(0 === false)        // false, not exactly the same
+console.log(0 == '')            // true, equivalent
+console.log(0 == ' ')           // true, equivalent
+console.log(0 === '')           // false, not exactly the same
+console.log(1 == true)          // true, equivalent
+console.log(1 === true)         // false, not exactly the same
+console.log(undefined == null)  // true
+console.log(undefined === null) // false
+console.log(NaN == NaN)         // false, not equal
+console.log(NaN === NaN)        // false
+console.log(typeof NaN)         // number
+
+console.log('mango'.length == 'avocado'.length)  // false
+console.log('mango'.length != 'avocado'.length)  // true
+console.log('mango'.length < 'avocado'.length)   // true
+console.log('milk'.length == 'meat'.length)      // true
+console.log('milk'.length != 'meat'.length)      // false
+console.log('tomato'.length == 'potato'.length)  // true
+console.log('python'.length > 'dragon'.length)   // false
+
+//Increment operator
+
+// 1 Pre-increment
+let count_0 = 0
+console.log(++count_0)
+console.log(count_0)
+
+// 2 Post-increment
+let count_1 = 0
+console.log(count_1++)
+console.log(count_1)
+
+//Decrement operator
+
+// 1 Pre-decrement
+let count_2 = 0
+console.log(--count_2)
+console.log(count_2)
+
+// 2 Post-decrement
+let count_3 = 0
+console.log(count_3--)
+console.log(count_3)
+
+//Ternary operators
+let is_raining = true
+is_raining ? console.log('You need a rain coat.') : console.log('No need for a rain coat.')
+
+isRaining = false
+
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
+
+  let number = 5
+number > 0
+  ? console.log(`${number} is a positive number`)
+  : console.log(`${number} is a negative number`)
+number = -5
+
+number > 0
+  ? console.log(`${number} is a positive number`)
+  : console.log(`${number} is a negative number`)
+
+  // Window methods
+  //1. alert
+  alert("hello")
+  
+  //2. prompt
+  prompt('required text','optional text')
+
+  let number_1 = prompt('Enter a number', 'number goes here')
+  console.log(number_1)
+
+  //3. confirm
+  const agree = confirm('Are you sure you like to delete?')
+  console.log(agree)
