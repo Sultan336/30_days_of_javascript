bd210f429d19d40fe855a00b3197920537171ce4
Sultan <m.sultan338@gmail.com>
2022-06-13 17:17:38 +0500

    level one

---
+++ 6/Exercises_Level_1.js
@@ -0,0 +1,63 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+// Iterate 0 to 10 using for loop, do the same using while and do while loop
+  // using for loop
+      for(i = 0; i <= 10; i++){
+        console.log(i)
+      }
+      console.log('Iterate 0 to 10 using for loop')
+
+      let j = 0
+      while (j <= 10){
+        console.log(j)
+        j++
+        
+      }
+      console.log('Iterate 0 to 10 using while loop')
+
+      let k = 0
+      do {
+        console.log(k)
+        k++
+      } while (k <= 10)
+      console.log('Iterate 0 to 10 using do while loop')
+
+     
+
+// Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+// Iterate 0 to n using for loop
+
+// Write a loop that makes the following pattern using console.log():
+
+//     #
+//     ##
+//     ###
+//     ####
+//     #####
+//     ######
+//     #######
